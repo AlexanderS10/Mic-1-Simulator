c3640a22a157dfdf29d161028f0f7a5396ac5df2
AlexanderS10 <57728893+AlexanderS10@users.noreply.github.com>
2019-11-19 23:36:02 -0500

    Update HighBit.java

--- HighBit.java
+++ HighBit.java
@@ -3,20 +3,20 @@ public class HighBit {
     private ControlLine n_cl;
     private ControlLine z_cl;
     private ControlLine jam_cl;
+    private boolean[] mpc= new boolean[]{};
 
-    public HighBit(ControlLine var1, ControlLine var2, ControlLine var3, ControlLine var4) {
-        this.mpc_cl = var4;
-        this.n_cl = var1;
-        this.z_cl = var2;
-        this.jam_cl = var3;
+    public HighBit(ControlLine n, ControlLine z, ControlLine jam, ControlLine mpc) {
+        this.mpc_cl = mpc;
+        this.n_cl = n;
+        this.z_cl = z;
+        this.jam_cl = jam;
+        defineHB();
     }
-
     public void defineHB(){
         boolean[] var1 = new boolean[]{this.jam_cl.getValue()[1] && this.n_cl.getValue()[0] || this.jam_cl.getValue()[2] && this.z_cl.getValue()[0]};
         this.mpc_cl.setValue(var1);
     }
-    public void poke() {
-        boolean[] var1 = new boolean[]{this.jam_cl.getValue()[0] && this.n_cl.getValue()[0] || this.jam_cl.getValue()[1] && this.z_cl.getValue()[0]};
-        this.mpc_cl.setValue(var1);
+    public ControlLine highBit_cl(){
+        return mpc_cl;
     }
 }
