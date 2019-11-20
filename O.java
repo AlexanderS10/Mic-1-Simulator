c5de1fc77b1803685ec583b3b8f5052db6bd1c3e
AlexanderS10 <57728893+AlexanderS10@users.noreply.github.com>
2019-11-19 23:38:36 -0500

    Update O.java

--- O.java
+++ O.java
@@ -3,28 +3,22 @@ public class O {
     private ControlLine mbr_cl;
     private ControlLine mpc_cl;
     private ControlLine jmpc_cl;
-
-    public O(ControlLine addr, ControlLine mbr, ControlLine jmpc, ControlLine mpc) {
+    private boolean[] MPC= new boolean[8];
+    public O(ControlLine addr, ControlLine mbr, ControlLine jmpc, ControlLine mpc){
         this.addr_cl = addr;
         this.mbr_cl = mbr;
         this.jmpc_cl = jmpc;
+        this.mpc_cl=mpc;
         defineO();
     }
-    public void defineO(){
+    private void defineO(){
         if(jmpc_cl.getValue()[0]){
-            int j=0;
-            boolean [] mpc= new boolean [8];
-            for(int i=1; i<8; i++){
-                mpc[j]=addr_cl.getValue()[i] || mbr_cl.getValue()[j];
-                j++;
+            for(int i=1; i<9; i++){
+                MPC[i-1]=addr_cl.getValue()[i] || mbr_cl.getValue()[i-1];
             }
-            mpc_cl.setValue(mpc);
-        }
-        else {
-            return;
         }
     }
-    public boolean[] getMPC(){
-        return this.mpc_cl.getValue();
+    public boolean[] getO(){
+        return this.MPC;
     }
 }
